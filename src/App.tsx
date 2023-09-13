752f9c5aa96c6329039ca4960744f36e494e0e35
João Guido <joaopgher@gmail.com>
2023-09-13 20:33:18 -0300

    new file:   .gitignore 	new file:   README.md 	new file:   package-lock.json 	new file:   package.json 	new file:   public/favicon.ico 	new file:   public/index.html 	new file:   public/logo192.png 	new file:   public/logo512.png 	new file:   public/manifest.json 	new file:   public/robots.txt 	new file:   src/App.tsx 	new file:   src/Style/style.css 	new file:   src/index.tsx 	new file:   src/logo.svg

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+//import { DrawerProvider } from './Context/DrawerContext';
+import "./Style/style.css";
+//import { Main } from './src/Components/Main';
+
+
+export default function App() {
+  return (
+    <div>
+      <p>Hello World!</p>
+    </div>
+    //<DrawerProvider>
+    // <Main />
+    //</DrawerProvider>
+  );
+};
